266e98cf3eb47054aec13e6e92f6f10d3e22cb83
caroline burns <carolineburns@carolines-MacBook.local>
2019-01-25 12:23:11 -0700

    set up knex environment

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-var port = process.env.PORT || 3000
+const knex = require('./knex')
+var port = process.env.PORT || 3002
 
 app.get('/', function(req, res, next){
      res.send('hello world')
 })
+
+const environment = process.env.NODE_ENV || 'development';
+const knexConfig = require('./knexfile')[environment];
+const db = require('knex')(knexConfig);
+
+const sql = knex('notecards').toString();
 
 app.listen(port, function() {
     console.log(`listening on ${port}`)
